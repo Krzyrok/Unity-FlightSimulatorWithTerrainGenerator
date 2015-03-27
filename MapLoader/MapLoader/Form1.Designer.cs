0ebb2c8c36b6bbad3e57e0f65a051c16490c9db7
heyDude <amba07dude@gmail.com>
2015-03-27 12:08:59 +0100

    Dodałem: * folder na jakieś dane (np. przykładowe mapy wysokościowe) * nowoutworzony projekt w Unity 5.0 (wstawiłem tam jakiś płaski teren) * programik w C# wczytujący dane z mapy wysokościowej (nie działa jeszcze do końca :P)
    Signed-off-by: heyDude <amba07dude@gmail.com>

---
+++ MapLoader/MapLoader/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace MapLoader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(23, 81);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(322, 200);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(52, 23);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(94, 41);
+            this.buttonSelect.TabIndex = 1;
+            this.buttonSelect.Text = "Select map file";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(226, 23);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(94, 41);
+            this.buttonSave.TabIndex = 3;
+            this.buttonSave.Text = "Save data";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 296);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.Text = "Map Loader";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button buttonSelect;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ MapLoader/MapLoader/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MapLoader
+{
+    public partial class Form1 : Form
+    {
+        int[] Pixels = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Bitmap bitmap = (Bitmap)Image.FromFile(openFileDialog1.FileName);
+
+                int width = bitmap.Width;
+                int height = bitmap.Height;
+
+                Pixels = null;
+                Pixels = new int[width * height + 2];
+                Pixels[0] = width;
+                Pixels[1] = height;
+
+                Color pixel;
+
+
+                for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                    //for (int x = 0; x < width; x++)
+                    {
+                        pixel = bitmap.GetPixel(x, y);
+                        Pixels[x + y + 2] = pixel.R;
+                    }
+
+                        bitmap.Dispose();
+
+                string text = "Map " + width + " x " + height + "  successfully loaded.\n";
+                Output.Text += text;
+            }
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            if(Pixels == null)
+                Output.Text += "There is no data to save.\n";
+            else
+            {
+                string filePath = "../../../../Data/map.txt";
+                System.IO.StreamWriter file = new System.IO.StreamWriter(filePath);
+
+                for (int i = 0; i < Pixels.Length; i++)
+                    file.WriteLine(Pixels[i]);
+
+                file.Close();
+
+                Output.Text += "Data successfully saved into file map.txt.\n";
+            }
+        }
+    }
+}
