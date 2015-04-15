131b27613d467e2e91017de08a5a1384f3a32932
heyDude <amba07dude@gmail.com>
2015-04-15 21:39:24 +0200

    Utworzenie dwóch scen i przejścia między nimi:
    1) Scena startowego menu wyboru mapy
    2) Scena symulacji z wygenerowanym terenem
    
    Signed-off-by: heyDude <amba07dude@gmail.com>

---
+++ UnityFlightSimulator/Assets/StartMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartMenu : MonoBehaviour {
+	
+	void Awake () {
+		Debug.Log ("Awake");
+	}
+	
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Start");
+	}
+	
+	// Update is called once per frame
+	void Update () {}
+	
+	void FixedUpdate() {}
+	
+	void OnEnable() {
+		Debug.Log ("OnEnable");
+	}
+	
+	void OnDisable() {
+		Debug.Log ("OnDisable");
+	}
+	
+	void OnGUI() {
+		if (GUI.Button (new Rect (0, 0, 100, 100), "START")) {
+			Debug.Log ("OnGUI");
+			Application.LoadLevel("SimulationScene");
+		}
+	}
+}
