c156674ae9cb77a3b44150906b34bbdcded9386d
Krzyrok <krzyrok@gmail.com>
2015-04-29 23:48:21 +0200

    dodany text dla punktacji (HUD) oraz kontroler dla niego - publiczna metoda dodajaca punkty i aktualizujaca wynik (AddToTheScore)

---
+++ project/UnityFlightSimulator/Assets/Scripts/Texts/ScoreTextController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreTextController : MonoBehaviour {
+	static int score;
+	static Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		scoreText = GetComponent<Text>();
+		scoreText.text = "Your score: " + score;
+	}
+
+	public static void AddScore(int points) {
+		score += points;
+		UpdateScoreText ();
+	}
+
+	private static void UpdateScoreText() {
+		scoreText.text = "Your score: " + score;
+	}
+}
