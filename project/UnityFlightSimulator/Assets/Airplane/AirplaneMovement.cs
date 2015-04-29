c5df8f7ceedce772b07f301870500c019923c57b
Krzyrok <krzyrok@gmail.com>
2015-04-29 20:25:57 +0200

    dodane sterowanie samolotem; zmiana w projekcie: Edit -> Project Settings -> Editor: 1) Version Control -> Mode: Visible Meta Files 2) Asset Serialization -> Mode: Force Text
    zmiana w .gitignore (by nie pokazywał w cholerę plików przy uruchomieniu Unity i commicie

---
+++ project/UnityFlightSimulator/Assets/Airplane/AirplaneMovement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirplaneMovement : MonoBehaviour {
+
+	public float AirplaneForwardSpeed;
+	public float AirplaneSteeringSpeed;
+
+	void Start () {
+	
+	}
+	
+	// Chnage on the FixedUpdate if will be used RgidBody
+	void Update () {
+		MoveForward ();
+		MoveInDirectionWantedByPlayer ();
+	}
+
+	void MoveForward() {
+		float movementSpeed = Time.deltaTime * AirplaneForwardSpeed;
+		transform.Translate(Vector3.forward * movementSpeed);
+	}
+
+	void MoveInDirectionWantedByPlayer(float movementSpeed) {
+		float translationHorizontal = Input.GetAxis("Horizontal") * AirplaneSteeringSpeed;
+		translationHorizontal *= Time.deltaTime;
+
+		float translationVertical = Input.GetAxis("Vertical") * AirplaneSteeringSpeed;
+		translationVertical *= Time.deltaTime;
+
+		transform.Translate(translationHorizontal, translationVertical, 0);
+	}
+}
