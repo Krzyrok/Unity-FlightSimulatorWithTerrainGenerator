fc67e6875394f1b87617602cdbd229e6a2c8387f
Krzyrok <krzyrok@gmail.com>
2015-05-05 20:48:38 +0200

    usuniete funkcje Start i Update z CollsionDetector

--- project/UnityFlightSimulator/Assets/Scripts/CollsionDetector.cs
+++ project/UnityFlightSimulator/Assets/Scripts/CollsionDetector.cs
@@ -3,14 +3,6 @@ using System.Collections;
 
 public class CollsionDetector : MonoBehaviour {
 
-	void Start () {
-	
-	}
-	
-	void Update () {
-	
-	}
-
 	void OnCollisionEnter(Collision collisionObject) {
 		if (collisionObject.gameObject.name == Constants.AirplaneObjectName) {
 			Destroy(collisionObject.gameObject);
