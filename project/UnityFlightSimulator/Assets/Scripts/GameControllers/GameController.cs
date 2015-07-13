fda29a5c8ef3f95541594273ba2de82e0e2cbef2
Krzyrok <krzyrok@gmail.com>
2015-07-13 02:28:15 +0200

    correct dealing with help text

--- project/UnityFlightSimulator/Assets/Scripts/GameControllers/GameController.cs
+++ project/UnityFlightSimulator/Assets/Scripts/GameControllers/GameController.cs
@@ -3,20 +3,16 @@ using System.Collections;
 
 public class GameController : MonoBehaviour {
 	private static bool _isGameOver = false;
+	private static bool _isGamePaused = false;
 	// Use this for initialization
 	void Start () {
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (_isGameOver && Input.GetKeyDown(KeyCode.R))
-		{
-			_isGameOver = false;
-			Time.timeScale = 1.0f;
-			Application.LoadLevel(Application.loadedLevel);
-		}
-
+		RestartIfNecessary ();
 		UpdateDisplayedTime ();
+		PauseOrResumeIfNecessary ();
 	}
 
 	public static void EndGame() {
@@ -24,8 +20,36 @@ public class GameController : MonoBehaviour {
 		Time.timeScale = 0.0f;
 	}
 
+	private void RestartIfNecessary()
+	{
+		if (_isGameOver && Input.GetKeyDown(KeyCode.R))
+		{
+			_isGameOver = false;
+			Time.timeScale = 1.0f;
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
 	private void UpdateDisplayedTime()
 	{
 		TimeTextController.UpdateTimeText (Time.time.ToString());
+	}
+
+	private void PauseOrResumeIfNecessary()
+	{
+		if (Input.GetKeyDown(KeyCode.F1))
+		{
+			if (_isGamePaused)
+			{
+				HelpTextController.HideHelpText();
+				Time.timeScale = 1.0f;
+			}
+			else
+			{
+				HelpTextController.ShowHelpText();
+				Time.timeScale = 0.0f;
+			}
+			_isGamePaused = !_isGamePaused;
+		}
 	}
 }
---
+++ project/UnityFlightSimulator/Assets/Scripts/Texts/HelpTextController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HelpTextController : MonoBehaviour {
+	private static Text _helpText;
+	private static string _helpTextForUser = "Try to collect all gates. Pass the green one.\n" +
+		"Green arrow points to the active gate.\n\n" +
+		"Press:\n" +
+		"Arrows to move\n" +
+		"1-4 to change camera\n" +
+		"Left ctrl to speed up\n" +
+		"Left alt to speed down";
+
+	// Use this for initialization
+	void Start () {
+		_helpText = GetComponent<Text>();
+	}
+
+	public static void ShowHelpText()
+	{
+		_helpText.text = _helpTextForUser;
+	}
+
+	public static void HideHelpText()
+	{
+		_helpText.text = "";
+	}
+}
